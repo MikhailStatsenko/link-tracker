d1feae64f756a70ac84d55aee235150ac8e00c4d
Mikhail Statsenko <112765729+MikhailStatsenko@users.noreply.github.com>
2024-04-14 23:11:37 +0300

    hw 9

--- bot/src/main/java/edu/java/bot/service/kafka/BotQueueConsumer.java
+++ bot/src/main/java/edu/java/bot/service/kafka/BotQueueConsumer.java
@@ -2,6 +2,7 @@ package edu.java.bot.service.kafka;
 
 import edu.java.bot.dto.request.LinkUpdateRequest;
 import edu.java.bot.service.UpdateService;
+import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,6 +19,7 @@ public class BotQueueConsumer {
         topics = "${app.kafka.topic-name}",
         groupId = "${app.kafka.consumer.group-id}"
     )
+    @Counted(value = "api_counter", description = "Amount of requests to API")
     public void listen(LinkUpdateRequest update) {
         try {
             updateService.processUpdate(update);
